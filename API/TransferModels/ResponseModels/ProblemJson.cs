184d0931799d10ba83add80fcdd0be506465c1f8
Carlos Florencio <carlosmflorencio@gmail.com>
2017-04-25 19:15:34 +0100

    Started Prroblem Json

---
+++ API/TransferModels/ResponseModels/ProblemJson.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.TransferModels.ResponseModels
+{
+    public class ProblemJson {
+
+        public const string MediaType = "application/problem+json";
+
+        public string Type { get; set; } // must be a uri
+        public string Title { get; set; }
+        public int Status { get; set; }
+        public string Detail { get; set; }
+        public string Instance { get; set; }
+        public List<InvalidParam> InvalidParams { get; set; }
+    }
+
+    public class InvalidParam
+    {
+        public string Name { get; set; }
+        public string Reason { get; set; }
+    }
+
+}
