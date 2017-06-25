5fca36575377ca82af8a31e5dbad61d5addb906a
no0law1 <nuno.m.reis@netcabo.pt>
2017-06-25 22:05:44 +0100

    Implemented get student with repository and get teacher without it(using href of homepage of the api)

--- app/src/components/students/Student.js
+++ app/src/components/students/Student.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+import { Segment, Card, Image } from 'semantic-ui-react'
+
 import StudentsRepository from '../../data/repositories/StudentsRepository'
 
 class Student extends Component {
@@ -11,14 +13,40 @@ class Student extends Component {
 
     componentDidMount() {
         StudentsRepository.getStudent(this.props.match.params.id)
-            .then(student => console.log(student))
+            .then(student => {
+                console.log(student)
+                this.setState({student})
+            })
     }
 
     render() {
+        const {student} = this.state
         return (
-            <div>
-                <h1>Student</h1>
-            </div>
+            <Segment basic textAlign='center'>
+                <Segment className='padding-left-right' color='teal'>
+                    <h1>Student</h1>
+                    {
+                        student &&
+                        <Card centered>
+                            <Image src='http://via.placeholder.com/300x150' />
+                            <Card.Content>
+                                <Card.Header>
+                                    {student.properties['name']}
+                                </Card.Header>
+                                <Card.Description>
+                                    Email: {student.properties['email']}
+                                </Card.Description>
+                            </Card.Content>
+                        </Card>
+                    }
+                    {
+                        //teacher &&
+                        //<DeleteTeacher 
+                        //    action={SirenHelpers.getAction(teacher, 'delete-teacher')}
+                        ///>
+                    }
+                </Segment>
+            </Segment>
         )
     }
 }
--- app/src/components/teachers/Teacher.js
+++ app/src/components/teachers/Teacher.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
-
 import axios from 'axios'
+
+import { Segment, Card, Image } from 'semantic-ui-react'
+
+//import SirenHelpers from '../../helpers/SirenHelpers'
 
 class Teacher extends Component {
     constructor(props) {
@@ -10,19 +13,47 @@ class Teacher extends Component {
     }
 
     componentDidMount() {
-        // TeacherRepository.getTeacher(this.props.match.params.id)
-        //     .then(teacher => console.log(teacher))
-        //     .catch(error => console.log(error.message))
-        axios.get('http://localhost:5000/api/teachers/'+this.props.match.params.id)
+        // First GET can be requested once in the begining and stored in redux store
+        axios.get()
             .then(resp => resp.data)
-            .then(teacher => console.log(teacher))
+            .then(home => axios.get(
+                home.TeacherEntry.replace("{number}", this.props.match.params.id))
+            )
+            .then(resp => resp.data)
+            .then(teacher => {
+                console.log(teacher)
+                this.setState({ teacher })
+            })
     }
 
     render() {
+        const { teacher } = this.state
         return (
-            <div>
-                <h1>Teacher</h1>
-            </div>
+            <Segment basic textAlign='center'>
+                <Segment className='padding-left-right' color='teal'>
+                    <h1>Teacher</h1>
+                    {
+                        teacher &&
+                        <Card centered>
+                            <Image src='http://via.placeholder.com/300x150' />
+                            <Card.Content>
+                                <Card.Header>
+                                    {teacher.properties['name']}
+                                </Card.Header>
+                                <Card.Description>
+                                    Email: {teacher.properties['email']}
+                                </Card.Description>
+                            </Card.Content>
+                        </Card>
+                    }
+                    {
+                        //teacher &&
+                        //<DeleteTeacher 
+                        //    action={SirenHelpers.getAction(teacher, 'delete-teacher')}
+                        ///>
+                    }
+                </Segment>
+            </Segment>
         )
     }
 }
